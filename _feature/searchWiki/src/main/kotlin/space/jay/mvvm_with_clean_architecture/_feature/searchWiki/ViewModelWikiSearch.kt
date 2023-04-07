5f5e49fdae8f386cd7def8f882c2273f2a081d68
Jay <blueriver.yu@gmail.com>
2023-04-08 00:46:45 +0900

    ui 상태 추가

--- _feature/searchWiki/src/main/kotlin/space/jay/mvvm_with_clean_architecture/_feature/searchWiki/ViewModelWikiSearch.kt
+++ _feature/searchWiki/src/main/kotlin/space/jay/mvvm_with_clean_architecture/_feature/searchWiki/ViewModelWikiSearch.kt
@@ -1,59 +1,64 @@
 package space.jay.mvvm_with_clean_architecture._feature.searchWiki
 
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import space.jay.mvvm_with_clean_architecture._core.common.wrapper.Fail
 import space.jay.mvvm_with_clean_architecture._core.common.wrapper.NetworkError
 import space.jay.mvvm_with_clean_architecture._core.common.wrapper.Success
 import space.jay.mvvm_with_clean_architecture._core.domain.UseCaseGetWikiData
-import space.jay.mvvm_with_clean_architecture._core.model.wiki.EntityWiki
-import java.util.*
+import space.jay.mvvm_with_clean_architecture._feature.searchWiki.state.ErrorMessage
+import space.jay.mvvm_with_clean_architecture._feature.searchWiki.state.StateUIWikiSearch
+import space.jay.mvvm_with_clean_architecture._feature.searchWiki.state.StateViewModelWikiSearch
 import javax.inject.Inject
 
 @HiltViewModel
 class ViewModelWikiSearch @Inject constructor(
-    val savedStateHandle : SavedStateHandle,
     private val getQueryWikiData : UseCaseGetWikiData,
 ) : ViewModel() {
 
-    companion object {
-        const val SEARCH_QUERY = "QUERY"
-    }
-
-    private val _uiState = MutableStateFlow(UiStateWiki())
-    val uiStateWiki : StateFlow<UiStateWiki> = _uiState
+    private val stateViewModel = MutableStateFlow(StateViewModelWikiSearch())
+    val stateUI : StateFlow<StateUIWikiSearch> = stateViewModel
+        .map(StateViewModelWikiSearch::toStateUi)
+        .stateIn(
+            viewModelScope,
+            SharingStarted.Eagerly,
+            stateViewModel.value.toStateUi()
+        )
 
     fun search(query : String?) {
-        savedStateHandle[SEARCH_QUERY] = query
-        query?.also {
-            _uiState.update {
-                UiStateWiki(isLoading = true)
+        query?.also { input ->
+            stateViewModel.update {
+                it.copy(
+                    searchInput = input,
+                    isLoading = true
+                )
             }
             viewModelScope.launch {
                 val result = getQueryWikiData(query)
-                _uiState.update {
-                    UiStateWiki().apply {
-                        when(result) {
-                            is Success -> this.data = result.data
-                            is NetworkError -> this.errorMessage = "${result.code} ${result.message}"
-                            is Fail -> this.errorMessage = "${result.throwable}"
-                        }
+                stateViewModel.update { state ->
+                    when (result) {
+                        is Success -> state.copy(isLoading = false, data = result.data)
+                        is NetworkError -> state.copy(
+                            isLoading = false,
+                            errorMessage = state.errorMessage + ErrorMessage(message = "${result.code} ${result.message}"),
+                            data = null
+                        )
+                        is Fail -> state.copy(
+                            isLoading = false,
+                            errorMessage = state.errorMessage + ErrorMessage(message = "${result.throwable}"),
+                            data = null
+                        )
                     }
                 }
             }
         }
     }
 }
-
-data class UiStateWiki(
-    var isLoading : Boolean = false,
-    var data : EntityWiki? = null,
-    var errorMessage: String? = null,
-    var id : Long = UUID.randomUUID().leastSignificantBits
-)
---
+++ _feature/searchWiki/src/main/kotlin/space/jay/mvvm_with_clean_architecture/_feature/searchWiki/state/StateViewModelWikiSearch.kt
@@ -0,0 +1,23 @@
+package space.jay.mvvm_with_clean_architecture._feature.searchWiki.state
+
+import space.jay.mvvm_with_clean_architecture._core.model.wiki.EntityWiki
+import java.util.UUID
+
+data class ErrorMessage(
+    val id : Long = UUID.randomUUID().mostSignificantBits,
+    val message : String
+)
+
+data class StateViewModelWikiSearch(
+    val searchInput : String = "",
+    val isLoading : Boolean = false,
+    val errorMessage : List<ErrorMessage> = emptyList(),
+    val data : EntityWiki? = null
+) {
+    fun toStateUi() : StateUIWikiSearch =
+        when {
+            isLoading -> StateUIWikiSearch.Loading(searchInput, errorMessage)
+            data != null -> StateUIWikiSearch.HasData(searchInput, errorMessage, data)
+            else -> StateUIWikiSearch.NoData(searchInput, errorMessage)
+        }
+}
