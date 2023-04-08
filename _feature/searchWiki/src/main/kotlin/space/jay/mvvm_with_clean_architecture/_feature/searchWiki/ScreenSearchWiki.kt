fcb0af1f6f0197c911c21341c4c12c809ddbdf84
Jay <blueriver.yu@gmail.com>
2023-04-08 14:11:49 +0900

    검색 로직 변경

--- _feature/searchWiki/src/main/kotlin/space/jay/mvvm_with_clean_architecture/_feature/searchWiki/ScreenSearchWiki.kt
+++ _feature/searchWiki/src/main/kotlin/space/jay/mvvm_with_clean_architecture/_feature/searchWiki/ScreenSearchWiki.kt
@@ -6,6 +6,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardActionScope
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -17,11 +20,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
+import space.jay.mvvm_with_clean_architecture._core.model.wiki.EntityWiki
 import space.jay.mvvm_with_clean_architecture._feature.searchWiki.state.StateUIWikiSearch
 
 @Composable
@@ -33,7 +38,8 @@ fun ScreenSearchWiki(
     Column(modifier = modifier) {
         SearchBarWiki(
             value = stateUI.searchInput,
-            onValueChange = viewModel::search
+            onValueChange = viewModel::search,
+            onSearch = { viewModel.search(stateUI.searchInput, true) }
         )
         SearchedWiki(stateUI)
     }
@@ -42,8 +48,9 @@ fun ScreenSearchWiki(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBarWiki(
-    value: String,
-    onValueChange: (String) -> Unit,
+    value : String,
+    onValueChange : (String) -> Unit,
+    onSearch : (KeyboardActionScope.() -> Unit)
 ) {
     val focus = remember { FocusRequester() }
     TextField(
@@ -52,6 +59,8 @@ fun SearchBarWiki(
             .focusRequester(focus),
         value = value,
         onValueChange = onValueChange,
+        keyboardActions = KeyboardActions(onSearch = onSearch),
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
         label = { Text(text = "Search Wiki") },
         singleLine = true,
     )
@@ -62,33 +71,33 @@ fun SearchBarWiki(
 
 @Composable
 fun SearchedWiki(stateUI : StateUIWikiSearch) {
-    when(stateUI) {
-        is StateUIWikiSearch.Loading -> Text(
-            modifier = Modifier
-                .padding(top = 16.dp)
-                .fillMaxSize(),
-            text = "Loading...",
-            textAlign = TextAlign.Center,
-        )
-        is StateUIWikiSearch.HasData -> {
-            val data = stateUI.data
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp, 8.dp)
-            ) {
-                item { Image(image = data.image) }
-                item { data.title?.also { Text(text = it, fontSize = 18.sp) } }
-                item { data.description?.also { Text(text = it) } }
-            }
-        }
-        is StateUIWikiSearch.NoData -> Text(
-            modifier = Modifier
-                .padding(top = 16.dp)
-                .fillMaxSize(),
-            text = "No data",
-            textAlign = TextAlign.Center,
-        )
+    when (stateUI) {
+        is StateUIWikiSearch.Loading -> SearchedWikiNoData(message = "Loading...")
+        is StateUIWikiSearch.HasData -> SearchedWikiHasData(data = stateUI.data)
+        is StateUIWikiSearch.NoData -> SearchedWikiNoData(message = "No Data")
+    }
+}
+
+@Composable
+fun SearchedWikiNoData(message: String) {
+    Text(
+        modifier = Modifier
+            .padding(top = 16.dp)
+            .fillMaxSize(),
+        text = message,
+        textAlign = TextAlign.Center,
+    )
+}
+@Composable
+fun SearchedWikiHasData(data : EntityWiki) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp, 8.dp)
+    ) {
+        item { Image(image = data.image) }
+        item { data.title?.also { Text(text = it, fontSize = 18.sp) } }
+        item { data.description?.also { Text(text = it) } }
     }
 }
 
