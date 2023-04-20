2c80f70dd14002f9deafbaf8d40db9934550c296
jay <nix@last30min.com>
2023-04-20 16:25:53 +0900

    DelayMessage 수정

--- core/common/src/main/kotlin/space/jay/mvvm_with_clean_architecture/core/common/delay/DelayMessage.kt
+++ core/common/src/main/kotlin/space/jay/mvvm_with_clean_architecture/core/common/delay/DelayMessage.kt
@@ -2,27 +2,21 @@ package space.jay.mvvm_with_clean_architecture.core.common.delay
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.mapLatest
-import kotlinx.coroutines.flow.merge
-import kotlinx.coroutines.flow.update
-import java.util.*
+import kotlinx.coroutines.flow.*
+import java.util.UUID
 
 @ExperimentalCoroutinesApi
 @FlowPreview
-class DelayMessage<T>(private val timeMillis : Long = 600) {
+class DelayMessage<T : Any>(timeMillis : Long = 600) {
 
     private val state = MutableStateFlow(StateDelayMessage<T>())
     private val flowDelay : Flow<T> = state
         .debounce(timeMillis)
         .filter { !it.isDirect && it.message != null }
-        .mapLatest { it.message!! }
+        .map { it.message!! }
     private val flowDirect : Flow<T> = state
         .filter { it.isDirect && it.message != null }
-        .mapLatest { it.message!! }
+        .map { it.message!! }
     val flowResult = merge(flowDelay, flowDirect)
 
     fun setMessage(content : T, isDirect : Boolean = false) {
--- core/common/src/main/kotlin/space/jay/mvvm_with_clean_architecture/core/common/delay/StateDelayMessage.kt
+++ core/common/src/main/kotlin/space/jay/mvvm_with_clean_architecture/core/common/delay/StateDelayMessage.kt
@@ -3,7 +3,7 @@ package space.jay.mvvm_with_clean_architecture.core.common.delay
 import java.util.UUID
 
 internal data class StateDelayMessage<T>(
-    val isDirect : Boolean = false,
+    val isDirect : Boolean = true,
     val message : T? = null,
     val id : Long = UUID.randomUUID().mostSignificantBits
 )
